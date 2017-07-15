2e914b7d3f2b404dd801fa20b13c1384546aa1b2
omkar pawar <omkarpawarfull@gmail.com>
2017-07-15 18:20:42 -0500

    Form with Text, Label
    Basic Form

---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,335 @@
+﻿namespace WindowsFormsApp2
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label7 = new System.Windows.Forms.Label();
+			this.label8 = new System.Windows.Forms.Label();
+			this.label9 = new System.Windows.Forms.Label();
+			this.label10 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.label11 = new System.Windows.Forms.Label();
+			this.label12 = new System.Windows.Forms.Label();
+			this.label13 = new System.Windows.Forms.Label();
+			this.label14 = new System.Windows.Forms.Label();
+			this.label15 = new System.Windows.Forms.Label();
+			this.panel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Minerva", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(124, 118);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(61, 25);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Name";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(215, 118);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(100, 22);
+			this.textBox1.TabIndex = 1;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(215, 188);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(100, 22);
+			this.textBox2.TabIndex = 3;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Minerva", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(124, 188);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(86, 25);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "Surname";
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(215, 255);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(100, 22);
+			this.textBox3.TabIndex = 5;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Minerva", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.Location = new System.Drawing.Point(124, 255);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(62, 25);
+			this.label3.TabIndex = 4;
+			this.label3.Text = "Phone";
+			// 
+			// textBox4
+			// 
+			this.textBox4.BackColor = System.Drawing.Color.White;
+			this.textBox4.Location = new System.Drawing.Point(215, 447);
+			this.textBox4.Multiline = true;
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(158, 112);
+			this.textBox4.TabIndex = 7;
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Font = new System.Drawing.Font("Minerva", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label4.Location = new System.Drawing.Point(124, 441);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(78, 25);
+			this.label4.TabIndex = 6;
+			this.label4.Text = "Address";
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Font = new System.Drawing.Font("Minerva", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label5.Location = new System.Drawing.Point(124, 351);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(40, 25);
+			this.label5.TabIndex = 8;
+			this.label5.Text = "City";
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Items.AddRange(new object[] {
+            "Mumbai",
+            "Banglore",
+            "Kashmir",
+            "Gujarat"});
+			this.comboBox1.Location = new System.Drawing.Point(215, 342);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(121, 24);
+			this.comboBox1.TabIndex = 9;
+			// 
+			// panel1
+			// 
+			this.panel1.Controls.Add(this.label15);
+			this.panel1.Controls.Add(this.label14);
+			this.panel1.Controls.Add(this.label13);
+			this.panel1.Controls.Add(this.label12);
+			this.panel1.Controls.Add(this.label11);
+			this.panel1.Controls.Add(this.label6);
+			this.panel1.Controls.Add(this.label7);
+			this.panel1.Controls.Add(this.label8);
+			this.panel1.Controls.Add(this.label9);
+			this.panel1.Controls.Add(this.label10);
+			this.panel1.Location = new System.Drawing.Point(681, 118);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(516, 464);
+			this.panel1.TabIndex = 10;
+			this.panel1.Visible = false;
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Font = new System.Drawing.Font("Minerva", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label6.Location = new System.Drawing.Point(50, 278);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(40, 25);
+			this.label6.TabIndex = 13;
+			this.label6.Text = "City";
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Font = new System.Drawing.Font("Minerva", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label7.Location = new System.Drawing.Point(50, 368);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(78, 25);
+			this.label7.TabIndex = 12;
+			this.label7.Text = "Address";
+			// 
+			// label8
+			// 
+			this.label8.AutoSize = true;
+			this.label8.Font = new System.Drawing.Font("Minerva", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label8.Location = new System.Drawing.Point(50, 182);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(62, 25);
+			this.label8.TabIndex = 11;
+			this.label8.Text = "Phone";
+			// 
+			// label9
+			// 
+			this.label9.AutoSize = true;
+			this.label9.Font = new System.Drawing.Font("Minerva", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label9.Location = new System.Drawing.Point(50, 115);
+			this.label9.Name = "label9";
+			this.label9.Size = new System.Drawing.Size(86, 25);
+			this.label9.TabIndex = 10;
+			this.label9.Text = "Surname";
+			// 
+			// label10
+			// 
+			this.label10.AutoSize = true;
+			this.label10.Font = new System.Drawing.Font("Minerva", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label10.Location = new System.Drawing.Point(50, 45);
+			this.label10.Name = "label10";
+			this.label10.Size = new System.Drawing.Size(61, 25);
+			this.label10.TabIndex = 9;
+			this.label10.Text = "Name";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(215, 623);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 11;
+			this.button1.Text = "Save";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// label11
+			// 
+			this.label11.AutoSize = true;
+			this.label11.BackColor = System.Drawing.Color.Aqua;
+			this.label11.Font = new System.Drawing.Font("Minerva", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label11.Location = new System.Drawing.Point(337, 45);
+			this.label11.Name = "label11";
+			this.label11.Size = new System.Drawing.Size(36, 25);
+			this.label11.TabIndex = 14;
+			this.label11.Text = "00";
+			// 
+			// label12
+			// 
+			this.label12.AutoSize = true;
+			this.label12.BackColor = System.Drawing.Color.Aqua;
+			this.label12.Font = new System.Drawing.Font("Minerva", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label12.Location = new System.Drawing.Point(337, 102);
+			this.label12.Name = "label12";
+			this.label12.Size = new System.Drawing.Size(36, 25);
+			this.label12.TabIndex = 15;
+			this.label12.Text = "00";
+			// 
+			// label13
+			// 
+			this.label13.AutoSize = true;
+			this.label13.BackColor = System.Drawing.Color.Aqua;
+			this.label13.Font = new System.Drawing.Font("Minerva", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label13.Location = new System.Drawing.Point(337, 182);
+			this.label13.Name = "label13";
+			this.label13.Size = new System.Drawing.Size(36, 25);
+			this.label13.TabIndex = 16;
+			this.label13.Text = "00";
+			// 
+			// label14
+			// 
+			this.label14.AutoSize = true;
+			this.label14.BackColor = System.Drawing.Color.Aqua;
+			this.label14.Font = new System.Drawing.Font("Minerva", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label14.Location = new System.Drawing.Point(337, 278);
+			this.label14.Name = "label14";
+			this.label14.Size = new System.Drawing.Size(36, 25);
+			this.label14.TabIndex = 17;
+			this.label14.Text = "00";
+			this.label14.Click += new System.EventHandler(this.label14_Click);
+			// 
+			// label15
+			// 
+			this.label15.AutoSize = true;
+			this.label15.BackColor = System.Drawing.Color.Aqua;
+			this.label15.Font = new System.Drawing.Font("Minerva", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label15.Location = new System.Drawing.Point(337, 368);
+			this.label15.Name = "label15";
+			this.label15.Size = new System.Drawing.Size(36, 25);
+			this.label15.TabIndex = 18;
+			this.label15.Text = "00";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1325, 702);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.textBox4);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Label label14;
+		private System.Windows.Forms.Label label13;
+		private System.Windows.Forms.Label label12;
+		private System.Windows.Forms.Label label11;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.Label label9;
+		private System.Windows.Forms.Label label10;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label15;
+	}
+}
+
---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			panel1.Visible = true;
+			label11.Text = textBox1.Text;
+			label12.Text = textBox2.Text;
+			label13.Text = textBox3.Text;
+			label14.Text = comboBox1.Text;
+			comboBox1.Items.Add(Text="Mumbai");
+			comboBox1.Items.Add(Text="Banglore");
+			label15.Text = textBox4.Text;
+		}
+
+		private void label14_Click(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
