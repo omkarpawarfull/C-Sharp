7ef2d5340eff1f08cbb93f6d918ec474dda6f432
omkar pawar <omkarpawarfull@gmail.com>
2017-07-16 17:27:56 -0500

    Opening New Form

---
+++ WindowsFormsApp6/WindowsFormsApp6/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Form2 myform = new Form2();
+            //myform.Show();
+            //this.Hide();
+
+            Form2 myform = new Form2();
+            myform.ShowDialog();
+            this.Hide();
+        }
+    }
+}
