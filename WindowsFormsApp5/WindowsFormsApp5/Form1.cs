a99e35e9a8ccd4f96c19383be844fdea1b0c713c
omkar pawar <omkarpawarfull@gmail.com>
2017-07-16 17:05:42 -0500

    Marksheet Form
    Includes if else and list

---
+++ WindowsFormsApp5/WindowsFormsApp5/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int exam1, exam2, avg;
+            exam1 = Convert.ToInt32(textBox2.Text);
+            exam2 = Convert.ToInt32(textBox3.Text);
+            avg = (exam1 + exam2) / 2;
+            listBox1.Items.Add(textBox1.Text);
+            listBox2.Items.Add(textBox2.Text);
+            listBox3.Items.Add(textBox3.Text);
+            listBox4.Items.Add(avg);
+            if (avg >= 50)
+            {
+                listBox5.Items.Add("Passed");
+            }
+            else {
+                listBox5.Items.Add("Failed");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+        }
+    }
+}
