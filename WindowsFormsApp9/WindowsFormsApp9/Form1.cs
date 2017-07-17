7ee6cbcbb0746dc42188f685f402b6c9a848ca83
omkar pawar <omkarpawarfull@gmail.com>
2017-07-17 16:10:34 -0500

    Random Number generation

---
+++ WindowsFormsApp9/WindowsFormsApp9/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            num = random.Next(0,21);
+            label1.Text = num.ToString();
+        }
+        int num;
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
