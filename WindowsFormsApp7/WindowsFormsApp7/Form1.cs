7ba0a55e196cf7712d59b8ddcedb58222c6bb006
omkar pawar <omkarpawarfull@gmail.com>
2017-07-17 14:16:27 -0500

    Admin Form

---
+++ WindowsFormsApp7/WindowsFormsApp7/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static String name, surname, city, other;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            name = textBox1.Text;
+            surname = textBox2.Text;
+            city = comboBox1.Text;
+            other = listBox1.Text;
+            Form2 myform = new Form2();
+            myform.ShowDialog();
+        }
+    }
+}
---
+++ WindowsFormsApp8/WindowsFormsApp8/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "admin" && textBox2.Text == "123456")
+            {
+                Form2 myform = new Form2();
+                myform.ShowDialog();
+            }
+            else {
+                MessageBox.Show("Username or Password not correct");
+            }
+        }
+    }
+}
