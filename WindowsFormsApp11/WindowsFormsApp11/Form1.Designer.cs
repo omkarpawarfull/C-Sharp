877572fca0c07cc6fe137e0781337aa265336661
omkar pawar <omkarpawarfull@gmail.com>
2017-07-17 16:55:46 -0500

    Dice Game

---
+++ WindowsFormsApp11/WindowsFormsApp11/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace WindowsFormsApp11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Location = new System.Drawing.Point(94, 128);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 84);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(53, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(75, 75);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(155, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(75, 75);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ WindowsFormsApp11/WindowsFormsApp11/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp11
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int dice1, dice2;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.ImageLocation = "C:\\Users\\omkar\\OneDrive\\Documents\\GitHub\\C-Sharp\\WindowsFormsApp11\\Dices\\1.png";
+            Random random = new Random();
+            dice1 = random.Next(1, 7);
+            dice2 = random.Next(1, 7);
+            switch (dice1) {
+                case 1: pictureBox1.ImageLocation = "C:\\Users\\omkar\\OneDrive\\Documents\\GitHub\\C-Sharp\\WindowsFormsApp11\\Dices\\1.png";
+                    break;
+                case 2:
+                    pictureBox1.ImageLocation = "C:\\Users\\omkar\\OneDrive\\Documents\\GitHub\\C-Sharp\\WindowsFormsApp11\\Dices\\2.png";
+                    break;
+                case 3:
+                    pictureBox1.ImageLocation = "C:\\Users\\omkar\\OneDrive\\Documents\\GitHub\\C-Sharp\\WindowsFormsApp11\\Dices\\3.png";
+                    break;
+                case 4:
+                    pictureBox1.ImageLocation = "C:\\Users\\omkar\\OneDrive\\Documents\\GitHub\\C-Sharp\\WindowsFormsApp11\\Dices\\4.png";
+                    break;
+                case 5:
+                    pictureBox1.ImageLocation = "C:\\Users\\omkar\\OneDrive\\Documents\\GitHub\\C-Sharp\\WindowsFormsApp11\\Dices\\5.png";
+                    break;
+                case 6:
+                    pictureBox1.ImageLocation = "C:\\Users\\omkar\\OneDrive\\Documents\\GitHub\\C-Sharp\\WindowsFormsApp11\\Dices\\6.png";
+                    break;
+            }
+            switch (dice2)
+            {
+                case 1:
+                    pictureBox2.ImageLocation = "C:\\Users\\omkar\\OneDrive\\Documents\\GitHub\\C-Sharp\\WindowsFormsApp11\\Dices\\1.png";
+                    break;
+                case 2:
+                    pictureBox2.ImageLocation = "C:\\Users\\omkar\\OneDrive\\Documents\\GitHub\\C-Sharp\\WindowsFormsApp11\\Dices\\2.png";
+                    break;
+                case 3:
+                    pictureBox2.ImageLocation = "C:\\Users\\omkar\\OneDrive\\Documents\\GitHub\\C-Sharp\\WindowsFormsApp11\\Dices\\3.png";
+                    break;
+                case 4:
+                    pictureBox2.ImageLocation = "C:\\Users\\omkar\\OneDrive\\Documents\\GitHub\\C-Sharp\\WindowsFormsApp11\\Dices\\4.png";
+                    break;
+                case 5:
+                    pictureBox2.ImageLocation = "C:\\Users\\omkar\\OneDrive\\Documents\\GitHub\\C-Sharp\\WindowsFormsApp11\\Dices\\5.png";
+                    break;
+                case 6:
+                    pictureBox2.ImageLocation = "C:\\Users\\omkar\\OneDrive\\Documents\\GitHub\\C-Sharp\\WindowsFormsApp11\\Dices\\6.png";
+                    break;
+            }
+
+        }
+    }
+}
