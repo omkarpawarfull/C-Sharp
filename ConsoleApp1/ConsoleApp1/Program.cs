c23a09a2953b3db316c9874814002a1a98a445a9
omkar pawar <omkarpawarfull@gmail.com>
2017-07-28 15:11:24 -0500

    Wpf trial

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Int32.Parse(Console.ReadLine());
+            int[] array = new int[n];
+            int total = 0;
+            for (int i = 0; i < n; i++) {
+                array[i] = Int32.Parse(Console.ReadLine());
+                if (array[i] % 2 == 0) {
+                    total = total + array[i];
+                }
+            }
+            Console.WriteLine("total is {0}",total);
+            Console.ReadLine();
+        }
+    }
+}
